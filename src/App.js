5ea10b6a3b145c0d0bf99aba354b0b232c163a07
unknown <vallefnd@gmail.com>
2022-01-20 13:19:38 -0300

    Algumas alterações pra fazer o esqueleto do trab, to mandando sem fazer de branch por causa de um erro q rolou aqui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components'
 
+const Root = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`
+const Filtros = styled.div`
+  border: 1px solid black;
+`
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const Produtos = styled.div`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: justify;
+  justify-content: space-between;
+  padding: 0px 16px;
+  border: 1px solid black;
+`
+
+const Carrinho = styled.div`
+  border: 1px solid black;
+`
+
+class App extends React.Component {
+
+  // state = {}
+
+  render() {
+    return (
+      <Root>
+        <Filtros><h3>Filtros:</h3></Filtros>
+        <Produtos><p>Quantidade de Produtos: </p> <p>Ordenação: </p></Produtos>
+        <Carrinho><h3>Carrinho:</h3></Carrinho>
+      </Root>    
+    );
+  }
 }
 
-export default App;
+export default App
